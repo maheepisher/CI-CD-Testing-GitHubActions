b2f92ec8c96688e7b31b820ad98d5dc40a984fb1
maheepisher <maheep.isher@gmail.com>
2025-02-23 01:55:54 -0500

    Added application & tests.

---
+++ tests/test_area.py
@@ -0,0 +1,19 @@
+import pytest  
+from src.area import calculate_area_square  
+  
+def test_calculate_area_square_negative():  
+    with pytest.raises(TypeError):  
+        calculate_area_square(-2)  
+  
+def test_calculate_area_square_string():  
+    with pytest.raises(TypeError):  
+        calculate_area_square("2")  
+  
+def test_calculate_area_square_list():  
+    with pytest.raises(TypeError):  
+        calculate_area_square([2])
+
+# New unit tests that validates the calculations done by your application are accurate.
+def test_calculate_area_square_valid():
+    assert calculate_area_square(7) == 25   #student id = 100909435
+    
